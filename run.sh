9d648dcf9cbc39d50704d1c288a48a8220bad2bd
Colin Sauze <colin.sauze@noc.ac.uk>
2023-09-22 17:11:38 +0100

    code to download and join the E3 and E4 tiles as used in the Haig Fras Digital Twin

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+mkdir emodnet
+cd emodnet
+# download the E3 tile
+wget https://downloads.emodnet-bathymetry.eu/v11/E3_2022.nc.zip 
+unzip E3_2022.nc.zip
+
+# download the E4 tile
+wget https://downloads.emodnet-bathymetry.eu/v11/E4_2022.nc.zip
+unzip E4_2022.nc.zip
+
+rm E4_2022.nc.zip E3_2022.nc.zip
+
+cd ..
+
+conda env update -f environment.yml
+conda activate imfe_convert_gebco
+
+python process_emodnet -i emodnet -o . --skip-upload
+
